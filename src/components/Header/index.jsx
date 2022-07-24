f9e79cf39326333bf1cedc5d8a37db79e3a56add
Thiago Nunes Batista <67487679+thiagonunesbatista@users.noreply.github.com>
2022-07-24 17:56:07 -0300

    feat: add header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,10 @@
 import styles from './styles.module.css'
 
+import IgniteLogo from '@/assets/icons/brands/ignite-logo.svg'
+
 export const Header = () => (
   <header className={styles.header}>
-    <p>Ignite Feed</p>
+    <img src={IgniteLogo} />
+    <p className={styles.header__title}>Ignite Feed</p>
   </header>
 )
