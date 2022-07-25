17a6d2d5eb4cf443972508eefb537dde6a73207e
Thiago Nunes Batista <67487679+thiagonunesbatista@users.noreply.github.com>
2022-07-25 12:16:00 -0300

    feat: add new layout improvement to post comments

--- src/components/Posts/Post/PostComments/Comment/LikeComment/index.jsx
+++ src/components/Posts/Post/PostComments/Comment/LikeComment/index.jsx
@@ -3,7 +3,10 @@ import styles from './styles.module.css'
 import Like from '@/assets/icons/actions/like.svg'
 
 export const LikeComment = ({ className }) => (
-  <button className={`${styles['like-comment']} ${className}`}>
+  <button
+    className={`${styles['like-comment']} ${className}`}
+    title='Aplaudir comentário'
+  >
     <img src={Like} />
     Aplaudir * 03
   </button>
--- src/components/Posts/Post/PostComments/Comment/index.jsx
+++ src/components/Posts/Post/PostComments/Comment/index.jsx
@@ -4,6 +4,10 @@ import { LikeComment } from './LikeComment'
 
 import styles from './styles.module.css'
 
+import IconTrash from '@/assets/icons/items/trash.svg'
+
+import { AuthorName } from '@/components/Author'
+
 export const Comment = ({
   comment: {
     content: { text },
@@ -12,7 +16,22 @@ export const Comment = ({
 }) => (
   <div className={styles.comment}>
     <ProfileAvatar profileName={name} profileAvatarUrl={profileAvatarUrl} />
-    <p className={styles.comment__text}>{text}</p>
+
+    <div className={styles.comment__content}>
+      <div className={styles.comment__header}>
+        <div>
+          <AuthorName name={name} />
+          <p className={styles['comment__published-at']}>Cerca de 2h</p>
+        </div>
+
+        <button className={styles.comment__delete} title='Deletar comentário'>
+          <img src={IconTrash} />
+        </button>
+      </div>
+
+      <p className={styles.comment__text}>{text}</p>
+    </div>
+
     <LikeComment className={styles.comment__like} />
   </div>
 )
